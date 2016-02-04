2c78078f1acaf3c1ebc285c0601dad6a04efb203
Sidhant Panda <sidhantpanda@gmail.com>
2016-02-05 04:28:42 +0530

    complete with gallery selector implementation

--- app/src/main/java/com/hashexclude/testapp/MainActivity.java
+++ app/src/main/java/com/hashexclude/testapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.hashexclude.testapp;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.hashexclude.simplegalleryselector.activities.ImageGalleryActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int IMAGE_GALLERY_RESULT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View btnGallery = findViewById(R.id.txt_image_gallery_selector);
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ImageGalleryActivity.class);
+                startActivityForResult(i, IMAGE_GALLERY_RESULT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_GALLERY_RESULT && resultCode == RESULT_OK) {
+            ClipData clip = data.getClipData();
+            if (clip != null) {
+                for (int i = 0; i < clip.getItemCount(); i++) {
+                    Uri uri = clip.getItemAt(i).getUri();
+                    Log.d(TAG, "File: " + uri.getPath());
+                }
+            }
+        }
     }
 }
